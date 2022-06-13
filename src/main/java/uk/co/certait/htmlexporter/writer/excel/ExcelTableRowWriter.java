57e84230fb7d3d1b36fa5e6cccf296928ebc6325
Alan Hay <alanhay99@hotmail.com>
2022-06-13 14:52:46 +0100

    Fix issue with borders on merged region after POI version upgrade.

--- src/main/java/uk/co/certait/htmlexporter/writer/excel/ExcelTableRowWriter.java
+++ src/main/java/uk/co/certait/htmlexporter/writer/excel/ExcelTableRowWriter.java
@@ -15,10 +15,9 @@
  */
 package uk.co.certait.htmlexporter.writer.excel;
 
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.util.CellRangeAddress;
-import org.apache.poi.ss.util.RegionUtil;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.jsoup.nodes.Element;
 
 import uk.co.certait.htmlexporter.writer.AbstractTableRowWriter;
@@ -42,20 +41,36 @@ public class ExcelTableRowWriter extends AbstractTableRowWriter {
 	}
 
 	public void doMerge(int rowIndex, int columnIndex, int rowSpan, int columnSpan) {
-		Cell cell = sheet.getRow(rowIndex).getCell(columnIndex);
+		XSSFCell cell = (XSSFCell) sheet.getRow(rowIndex).getCell(columnIndex);
 		CellRangeAddress range = new CellRangeAddress(rowIndex, rowIndex + rowSpan - 1, columnIndex,
 				columnIndex + columnSpan - 1);
 
+		// The code here previously relied on RegionUtils.(...) to set the border for
+		// the merged range however that does not seem to have been updated to handle
+		// the new XssfColor model and borders in merged regions are therefore always
+		// rendered as black.
+		// In order to have the borders for a merged region correctly rendered, it looks
+		// like we first have to set the border on each individual cell before calling
+		// the merge operation which seems to be broadly what RegionUtils was doing.
+		// Here we just copy the entire style object from the first cell to the other
+		// cells as there does not seem to be a case when that is not what we would
+		// want.
+
+		for (int row = rowIndex; row < rowIndex + rowSpan; ++row) {
+			if (sheet.getRow(row) == null) {
+				sheet.createRow(row);
+			}
+
+			for (int col = columnIndex; col < columnIndex + columnSpan; ++col) {
+				XSSFCell nextCell = (XSSFCell) sheet.getRow(row).getCell(col);
+
+				if (nextCell == null) {
+					nextCell = (XSSFCell) sheet.getRow(row).createCell(col);
+				}
+				nextCell.setCellStyle(cell.getCellStyle());
+			}
+		}
+
 		sheet.addMergedRegion(range);
-
-		RegionUtil.setBorderBottom(cell.getCellStyle().getBorderBottom(), range, sheet);
-		RegionUtil.setBorderTop(cell.getCellStyle().getBorderTop(), range, sheet);
-		RegionUtil.setBorderLeft(cell.getCellStyle().getBorderLeft(), range, sheet);
-		RegionUtil.setBorderRight(cell.getCellStyle().getBorderRight(), range, sheet);
-
-		RegionUtil.setBottomBorderColor(cell.getCellStyle().getBottomBorderColor(), range, sheet);
-		RegionUtil.setTopBorderColor(cell.getCellStyle().getTopBorderColor(), range, sheet);
-		RegionUtil.setLeftBorderColor(cell.getCellStyle().getLeftBorderColor(), range, sheet);
-		RegionUtil.setRightBorderColor(cell.getCellStyle().getRightBorderColor(), range, sheet);
 	}
 }
